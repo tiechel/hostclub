e5bb253c475d2c09da455dad0abbae29e75eef1b
tiechel <alicetype.luna@gmail.com>
2020-10-23 02:06:15 +0900

    スタイル調整

--- hostclub/views/__init__.py
+++ hostclub/views/__init__.py
@@ -4,3 +4,8 @@ from django.views.generic import TemplateView
 
 class TopView(TemplateView):
     template_name = 'hostclub/top.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'トップページ'
+        return context
